7c759ddacadb60d0d73c0d0f513d73c381c2e814
wizi-tag <a.kapyash@gmail.com>
2022-09-16 23:18:44 +0800

    completed

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main class="content">
+
+      <Board id="first">
+
+        <Card id="task1" draggable="true">
+          <p>Card_1</p>
+        </Card>
+
+        <Card id="task2" draggable="true">
+          <p>Card_2</p>
+        </Card>
+
+        <Card id="task3" draggable="true">
+          <p>Card_3</p>
+        </Card>
+
+      </Board>
+
+      <Board id="second"></Board>
+
+      <Board id="third"></Board>
+
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Board from './components/dnd-board';
+import Card from './components/dnd-card';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Board,
+    Card
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background-color: #D0D0C0;
+  }
+
+  .content{
+    display: flex;
+    jusitfy-content: space-between;
+
+    width: 100%;
+    max-width: 70%;
+    height: 100vh;
+
+    overflow: hidden;
+
+    margin: 0 auto;
+    padding: 15px;
+  }
+
+  .content .board{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 300px;
+    background-color: #313131;
+    padding: 15px;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 15px;
+  }
+
+  .content .board .card{
+    padding: 15px 25px;
+    background-color: #F3F3F3;
+    cursor: pointer;
+    margin-bottom: 15px;
+    border-radius: 5px;
+    text-align: center;
+  }
+  #first .card{
+    background-color: #990000;
+    color: black;
+  }
+
+  #third .card{
+    background-color: #009900;
+    color: white;
+  }
+
 </style>
---
+++ src/components/dnd-card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    :id="id"
+    class="card"
+    :draggable="draggable"
+    @dragstart="dragStart"
+    @dragover.stop
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id', 'draggable'],
+  methods: {
+    dragStart: e => {
+      const target = e.target;
+      e.dataTransfer.setData('card_id', target.id);
+      setTimeout(() => {}, 0)
+    }
+  }
+}
+</script>
